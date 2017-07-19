373580e51dae8650f22b13118aaea6f401369ec7
juju4 <juju4@users.noreply.github.com>
2017-07-18 20:20:18 -0400

    grr tests review

--- test/integration/default/serverspec/grr_spec.rb
+++ test/integration/default/serverspec/grr_spec.rb
@@ -34,11 +34,11 @@ end
 
 
 ### Note: only valid after 'grr_config_updater initialize'
-#describe service('grr-server'), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
-#  it { should be_enabled }
-#  it { should be_running }
-#end
-#
+describe service('grr-server'), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
 #describe service('grr-dataserver-master'), :if => os[:family] == 'ubuntu' && os[:release] != '16.04' do
 #  it { should be_enabled }
 ##  it { should be_running }
@@ -64,10 +64,10 @@ end
 #  it { should be_running }
 #end
 #
-#describe process("grr_server") do
-#
-#  its(:user) { should eq "root" }
-#
+describe process("grr_server") do
+
+  its(:user) { should eq "root" }
+
 #  it "is listening on port 44449" do
 #    expect(port(44449)).to be_listening
 #  end
@@ -77,26 +77,29 @@ end
 #  it "is listening on port 8080" do
 #    expect(port(8080)).to be_listening
 #  end
-#
-#end
-#
-#describe file('/var/log/syslog') do
-#  it { should exist }
-#  its(:content) { should_not match /grr_server.*ERROR/ }
-#end
-#
+
+end
+
+describe file('/var/log/syslog'), :if => os[:family] == 'ubuntu' do
+  it { should exist }
+  its(:content) { should_not match /grr_server.*ERROR/ }
+end
+describe file('/var/log/messages'), :if => os[:family] == 'redhat' do
+  it { should exist }
+  its(:content) { should_not match /grr_server.*ERROR/ }
+end
+
 ### only in master
 ##describe command('/bin/bash run_tests.sh | tee /root/grr-run_tests.log'), :sudo => false do
 ###  its(:stdout) { should match /bin/ }
 ##  its(:exit_status) { should eq 0 }
 ##end
-#
-#describe command('curl -sSq http://127.0.0.1:8000/server.pem') do
-#  its(:stdout) { should match /-----BEGIN CERTIFICATE-----/ }
-#  its(:exit_status) { should eq 0 }
-#end
-#describe command('curl -sSq http://127.0.0.1:8000/control') do
-#  its(:stdout) { should match /Empty reply from server/ }
-#  its(:exit_status) { should eq 52 }
-#end
-#
+
+describe command('curl -sSq http://127.0.0.1:8000/server.pem') do
+  its(:stdout) { should match /-----BEGIN CERTIFICATE-----/ }
+  its(:exit_status) { should eq 0 }
+end
+describe command('curl -sSq http://127.0.0.1:8000/control') do
+  its(:stderr) { should match /Empty reply from server/ }
+  its(:exit_status) { should eq 52 }
+end
--- test/integration/default/serverspec/rekall_spec.rb
+++ test/integration/default/serverspec/rekall_spec.rb
@@ -5,7 +5,7 @@ require 'serverspec'
 set :backend, :exec
 
 #describe file('/usr/local/bin/rekall') do
-describe file('/root/env-grr/bin/rekall') do
+describe file('/usr/local/share/env-rekall/bin/rekall') do
   it { should exist }
   it { should be_executable }
 end
