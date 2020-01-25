6e2bc710c3df30c8460762fcffcc160c431238cd
juju4 <juju4@users.noreply.github.com>
2020-01-25 15:03:09 -0500

    use geerlingguy.mysql

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,6 +11,7 @@ fi
 
 [ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/juju4.rekall ] && git clone https://github.com/juju4/ansible-rekall $rolesdir/juju4.rekall
+[ ! -d $rolesdir/geerlingguy.mysql ] && git clone https://github.com/geerlingguy/ansible-role-mysql $rolesdir/geerlingguy.mysql
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.grr ] && ln -s ansible-grr $rolesdir/juju4.grr
 [ ! -e $rolesdir/juju4.grr ] && cp -R $rolesdir/ansible-grr $rolesdir/juju4.grr
